e2b6768bbcfc303126c3a81c9546f2a39ae53444
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-19 20:14:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each{|names| arr << "Hello, my name is #{names}."}
+  arr
+end
+
+def assign_rooms(array)
+  arr = []
+  array.each_with_index{|name, index| arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  arr
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|names| puts names}
+  assign_rooms(attendees).each{|rooms| puts rooms}
+end
